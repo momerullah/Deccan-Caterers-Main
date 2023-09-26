27e2f11c21b0b5f4cd89c5236edff42d86404332
Omer <85346586+momerullah@users.noreply.github.com>
2023-09-26 00:07:25 -0500

    Updated Contact Info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
         <section id="contact" className="info-section">
             <div className="contact-section">
                 <h2>Contact Us</h2>
-                <p>Contact Name: Wajahat Ali</p>
+                <p>Contact Name: Amena Hussaini</p>
                 <p>Phone: <a href="tel:630-426-7001">630-426-7001</a></p>
-                <p>Email: <a href="mailto:wajahat.ali@outlook.com">wajahat.ali@outlook.com</a></p>
+                <p>Email: <a href="mailto:amenahussaini@outlook.com">amenahussaini@outlook.com</a></p>
                 <p>We are always happy to assist you with your queries and orders. Feel free to contact us for any information or assistance you may need.</p>
             </div>
         </section>
